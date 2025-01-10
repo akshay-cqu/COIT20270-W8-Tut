80f75009c0ef7accf5322456fe2092a3cc714e6e
akshay-cqu <akshay.benny@cqumail.com>
2025-01-11 09:30:17 +1100

    Initial commit

---
+++ BurgerRecipe/BurgerRecipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerRecipe
+//
+//  Created by user256122 on 1/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
